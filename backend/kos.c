2a4d2b9edd6b6830b9d3c7960a8b65236082c643
KosulGurung <61305786+Kosul58@users.noreply.github.com>
2025-03-06 12:15:59 +0545

    Update kos.c

--- backend/kos.c
+++ backend/kos.c
@@ -1,6 +1,1 @@
-MONGODB_URI = "mongodb+srv://kosul:kosul@cluster0.jn30nsv.mongodb.net/chatapp?retryWrites=true&w=majority&appName=chatApp"
 
-
-CLOUDINARY_CLOUD_NAME='dyhju2mcs'
-CLOUDINARY_API_KEY='484822588963412'
-CLOUDINARY_API_SECRET='teb4pGz2Mqmk0-8lNgo29LfsMSI'
