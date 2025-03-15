d222f6f37de590f287957e479516f750f61fd7a0
Kosul58 <kosulgrg@gmail.com>
2025-03-15 13:59:12 +0545

    a

--- backend/index.js
+++ backend/index.js
@@ -41,8 +41,15 @@ const onlineUsers = new Map(); // Store online users
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // Adjust for frontend URL
+    origin: [
+      "http://localhost:5174",
+      "http://127.0.0.1:5174",
+      "http://localhost:5173",
+      "https://chat-app-vue-js.vercel.app/",
+    ], // Add allowed frontend URLs
     methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"], // Add headers if needed
+    credentials: true, // Allow cookies if necessary
   },
 });
 
